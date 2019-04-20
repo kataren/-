1819e76af1a3ed33f58e298d3303915320215e24
kataren <49614945+kataren@users.noreply.github.com>
2019-04-20 12:34:55 +0900

    Update kataren.py

--- kataren.py
+++ kataren.py
@@ -16,7 +16,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('>안녕'):
+    if message.content.startswith('!안녕'):
         await message.channel.send("안녕하세요?")
 
 
